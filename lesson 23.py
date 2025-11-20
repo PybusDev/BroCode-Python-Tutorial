d0323d5bfcbe2721ebd1a3444014ca6d8e2eb081
Chris Pybus <GitChris@Pybus.Dev>
2025-11-20 10:33:44 -0600

    Add lesson 23

---
+++ lesson 23.py
@@ -0,0 +1,37 @@
+# 2D Lists
+
+# fruits = ["apple", "orange", "banana", "coconut"]
+# vegetables = ["celery", "carrots", "potatoes"]
+# meats = ["chicken", "fish", "turkey", "ham", "pork"]
+
+
+# groceries = [fruits, vegetables, meats]
+
+# print(groceries[0][0])
+
+
+# groceries = [["apple", "orange", "banana", "coconut"], 
+#              ["celery", "carrots", "potatoes"], 
+#              ["chicken", "fish", "turkey", "ham", "pork"]]
+
+
+# for collection in groceries:
+#     for food in collection:
+#         print(food, end=" ")
+#     print()
+
+
+
+# ============ EXERCISE 1 ================
+
+
+num_pad = ((1, 2, 3),
+           (4, 5, 6),
+           (7, 8, 9),
+           ("*", 0, "#"))
+
+
+for row in num_pad:
+    for num in row:
+        print(num, end=" ")
+    print()
