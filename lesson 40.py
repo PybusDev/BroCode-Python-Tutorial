e83ee20db6e8edede8a773d9146efe160a3a343e
Chris Pybus <GitChris@Pybus.Dev>
2025-11-22 15:22:52 -0600

    Add lesson 40 and extra parts from lesson

---
+++ lesson 40.py
@@ -0,0 +1,18 @@
+# module = a file conmtaining code you want to include in your program
+#          use 'import' to include a module (built-in or your own)
+#          useful to break up a large program reusable separate files
+
+import example
+
+result = example.pi
+result2 = example.square(3)
+result3 = example.cube(3)
+result4 = example.circumference(3)
+result5 = example.area(3)
+
+print(result)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
+
