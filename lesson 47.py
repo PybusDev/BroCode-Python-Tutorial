d384f7a9aaa5b38439304af4a6aaa80f95ffb26d
Chris Pybus <GitChris@Pybus.Dev>
2025-11-26 14:58:25 -0600

    Add Lesson 47 with corresponding parts

--- lesson 47.py
+++ lesson 47.py
@@ -6,6 +6,13 @@
 
 from car import Car
 
+# class Car:
+#    def __init__(self, model, year, color, for_sale):
+#        self.model = model
+#        self.year = year
+#        self.color = color
+#        self.for_sale = for_sale
+
 car1 = Car("Mustang", 2024, "red", False)
 car2 = Car("Corvette", 2025, "blue", True)
 car3 = Car("Charger", 2026, "yellow", True)
